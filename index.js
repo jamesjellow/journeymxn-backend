a772959f575667be3c845e2bc7e36fb5c83e183b
jamesjellow <jellowjames@gmail.com>
2021-03-04 16:01:07 -0800

    Template for questions, asnwers, and skills

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 let port = process.env.PORT || 3000;
 const importData = require('./data.json')
+const careerQuestions = require('./models/questions.js')
 
+// console.log(careerQuestions)
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send(careerQuestions)
+    // res.send('Hello World');
 })
 
 app.get('/players', (req, res) => {
