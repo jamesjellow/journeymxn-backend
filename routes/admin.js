f091ce24182a82ac1345dfa6ae858884afdc1bb9
Mugdhaa Patankar <mpatanka@uci.edu>
2021-04-21 19:25:14 -0700

    Modified admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,8 @@ function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated())
       return next();
     else
-    console.log("Unauthorized request. Redirecting to /login");
-    return res.redirect('/login');
+    {console.log("Unauthorized request. Redirecting to /login");
+    return res.redirect('/login');}
 }
 
 router.get("/",ensureAuthenticated, (req, res) => {
@@ -14,8 +14,10 @@ router.get("/",ensureAuthenticated, (req, res) => {
     // console.log(req.session);
     // console.log(req.user);
     if (req.user.role == 'admin' && req.session.passport.user == req.user.id)
-    console.log("Request re-authenticated in GET method for /admin. Login Successful!")
-    return res.status(301).send('Access Granted! Welcome to Admin Page!');
+    {
+        console.log("Request re-authenticated in GET method for /admin. Login Successful!")
+        return res.status(301).send('Access Granted! Welcome to Admin Page!');
+    }
 });
 
 module.exports = router;
